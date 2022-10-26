975f73ba66a3423b69a2dc864c2f1009221d4201
Alloutnoob <bryanburgess95@hotmail.com>
2022-10-26 03:52:35 -0500

    Add commands to PLC project

--- Control_Cabinets/Control Panel.py
+++ Control_Cabinets/Control Panel.py
@@ -1,10 +1,28 @@
+from imp import get_tag
 from pylogix import PLC
 
 PLCIP = input("PLC IP Address", )
 
-with PLC(PLCIP) as comm:
-    comm.IPAddress = '10.8.236.183'
-    tags = comm.GetTagList(False)
+
+def simple_read_tag():
+    with PLC() as comm:
+        comm.IPAddress = PLCIP
+        Tag_Name = input("Tag Name", )
+        ret = comm.Read(Tag_Name)
+    print(ret.Value)
+
+def simple_write_tag():
+    with PLC() as comm:
+        comm.IPAddress = PLCIP
+        Tag_Name = input("Tag Name", )
+        comm.Write(Tag_Name, 10)
+
+
+def get_tag_list():
+    with PLC() as comm:
+        comm.IPAddress = PLCIP
+        tags = comm.GetTagList(False)
     
     for t in tags.Value:
         print(t.TagName, t.DataType)
+
