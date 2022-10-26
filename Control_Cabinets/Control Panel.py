62b03d70d6b1c3b0d541842b7c6bbe22a24e2bf5
Alloutnoob <bryanburgess95@hotmail.com>
2022-10-26 04:22:31 -0500

    Update PLC Project

--- Control_Cabinets/Control Panel.py
+++ Control_Cabinets/Control Panel.py
@@ -18,11 +18,35 @@ def simple_write_tag():
         comm.Write(Tag_Name, 10)
 
 
-def get_tag_list():
+def get_controller_tag_list():
     with PLC() as comm:
         comm.IPAddress = PLCIP
         tags = comm.GetTagList(False)
-    
     for t in tags.Value:
-        print(t.TagName, t.DataType)
+        print(t.TagName)
+        
+def get_program_tag_list():
+    with PLC() as comm:
+        comm.IPAddress = PLCIP
+        tags = comm.GetProgramTagList('Program:MiscHMI')
+    for t in tags.Value:
+        print(t.TagName)
 
+def device_discovery():
+    with PLC() as comm:
+        devices = comm.Discover()
+    for device in devices.Value:
+        print(device.IPAddress)
+        print('  Product Code: ' + device.ProductName + ' ' + str(device.ProductCode))
+        print('  Vendor/Device ID:' + device.Vendor + ' ' + str(device.DeviceID))
+        print('  Revision/Serial:' +  device.Revision + ' '  + device.SerialNumber)
+        print('')
+
+def save_tag_list():
+    with PLC() as comm:
+        comm.IPAddress = PLCIP
+        tags = comm.GetTagList()
+
+    with open('tag_list.txt', 'w') as f:
+        for t in tags.Value:
+            f.write('%s %d \n'.format(t.TagName, t.DataType))
