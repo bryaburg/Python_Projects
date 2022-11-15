96dd9ebc82c588b45ce7a9b31726563f8dde3ed8
Alloutnoob <bryanburgess95@hotmail.com>
2022-11-14 20:14:06 -0600

    Remove extra imports not needed

--- Good Catch Near Miss/Good_Catch_Near_Miss.py
+++ Good Catch Near Miss/Good_Catch_Near_Miss.py
@@ -4,8 +4,6 @@ from selenium.webdriver.chrome.service import Service as ChromiumService
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.core.utils import ChromeType
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import Select
 import datetime
 import time
 import calendar
