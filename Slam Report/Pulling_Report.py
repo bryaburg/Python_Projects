92507e8b75787861fd836be953b49344e3bf0cd7
Alloutnoob <bryanburgess95@hotmail.com>
2023-01-08 09:47:48 -0600

    Adding comments to slam report

--- Slam Report/Pulling_Report.py
+++ Slam Report/Pulling_Report.py
@@ -13,7 +13,7 @@ goog_url = "https://monitorportal.amazon.com/mws?Action=GetGraph&Version=2007-07
            "%20&Stat2=n&HeightInPixels=250&WidthInPixels=600&GraphTitle=SLAM%20REPORT%20v5&TZ=CST6CDT@TZ%3A%20CST6CDT" \
            "&StartTime1=-PT12H&EndTime1=-PT0H&OutputFormat=CSV_TRANSPOSE "
 
-
+# This grabs the slam data and saves it in a CSV file
 def download_slam_data(csv_url):
     download = requests.get(csv_url, auth=HTTPKerberosAuth(OPTIONAL), verify=False)
 
@@ -25,12 +25,12 @@ def download_slam_data(csv_url):
     df = pd.DataFrame(my_list)
     df.to_csv("SLAM_file.csv")
     
-# Read the CSV file
+# Read the CSV file and drop the first column
     df = pd.read_csv('SLAM_file.csv')
     df.drop(df.columns[0], axis=1, inplace=True)
 
 
-# Open the existing Excel file
+# Open the existing Excel file and put in csv data
     with pd.ExcelWriter('C:/Users/bryaburg/Desktop/Python_Projects/Python_Projects/Slam Report/SLAM Report.xlsx', mode='a', engine= "openpyxl", if_sheet_exists = 'replace') as writer:
         # Write the data to the specified sheet
         df.to_excel(writer,sheet_name='DATA', startrow=0, startcol=0,  index=False, header=False)
@@ -43,6 +43,8 @@ def download_slam_data(csv_url):
 
 download_slam_data(goog_url)
 
+
+# This will grab the xlsx file I just made and put it in webhook URL
 webhook_url = "https://hooks.slack.com/services/XXX/XXX/XXX"
 
 def send_file_to_webhook(webhook_url):
