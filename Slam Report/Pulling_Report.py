5b66463e4129a636a7021836059c549fa18b3762
Alloutnoob <bryanburgess95@hotmail.com>
2023-01-12 02:44:49 -0600

    Slam Report Update

--- Slam Report/Pulling_Report.py
+++ Slam Report/Pulling_Report.py
@@ -15,29 +15,26 @@ goog_url = "https://monitorportal.amazon.com/mws?Action=GetGraph&Version=2007-07
 
 # This grabs the slam data and saves it in a CSV file
 def download_slam_data(csv_url):
+    
+    #This Auth me getting the info from the URL
     download = requests.get(csv_url, auth=HTTPKerberosAuth(OPTIONAL), verify=False)
-
+    
+    # This decodes it and then saves it to a CSV file
     decoded_csv = download.content.decode('utf-8')
     cr = csv.reader(decoded_csv.splitlines(), delimiter=',')
     my_list = list(cr)
-    for row in my_list:
-        print(my_list)
     df = pd.DataFrame(my_list)
     df.to_csv("SLAM_file.csv", header=False, index=False)
-    
-# Read the CSV file and drop the first column
-    df = pd.read_csv('SLAM_file.csv')
-    #df.drop(df.columns[0], axis=1, inplace=True)
-    s = df.loc[4:17]
-    df[s] = df[s].astype("float")
-    results = df.dtypes
-    print(results)
 
-
-# Open the existing Excel file and put in csv data
+    # Open the existing Excel file and put in csv data
     with pd.ExcelWriter('C:/Users/bryaburg/Desktop/Python_Projects/Python_Projects/Slam Report/SLAM Report.xlsx', mode='a', engine= "openpyxl", if_sheet_exists = 'replace') as writer:
+        # Read the excel file
+        df = pd.read_csv('SLAM_file.csv')
+        #Convert data in Dataframe to float
+        df = df.applymap(float)
         # Write the data to the specified sheet
         df.to_excel(writer,sheet_name='DATA', startrow=0, startcol=0,  index=False, header=False)
+
 
 
 download_slam_data(goog_url)
