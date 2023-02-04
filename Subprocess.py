79c5a0e1380352a9e3d6325f3f7256e0298b3d32
Alloutnoob <bryanburgess95@hotmail.com>
2023-02-04 04:29:02 -0600

    Password Code

---
+++ Subprocess.py
@@ -0,0 +1,33 @@
+import subprocess
+
+meta_data = subprocess.check_output(['netsh', 'wlan', 'show', 'profiles'])
+data = meta_data.decode('utf-8', errors = "backslashreplace")
+data = data.split('\n')
+profiles = []
+
+for i in data:
+    if "All User Profiles" in i :
+        i = i.split(":")
+        i = i[i]
+        i = i[1:-1]
+        profiles.append(i)
+        
+print("{:30}| {:<}".format("Wifi Name", "Password"))
+print("-------------------------------------------")
+
+for i in profiles:
+    
+    try:
+    
+        try:
+        
+            results = subprocess.check_output(['netsh', 'wlan', 'show', 'profile', i , 'key = clear'])
+            results = results.decode('utf-8', errors = "backslashreplace")
+            results = results.split('\n')
+            results = [b.split(":")[1][1:-1] for b in results if "Key Content" in b]
+        
+        except IndexError:
+            print("{:<30}| {:<}".format(i,""))
+        
+    except subprocess.CallProcessError:
+        print("Encoding Error Occurred")
