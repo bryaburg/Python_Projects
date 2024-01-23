38a80066f76b05972b22f07a69b66e275fc58ab6
alloutnoob <bryanburgess95@hotmail.com>
2024-01-23 08:36:25 -0600

    Cleaning up logix code

--- logix500to5000tags/formatter.py
+++ logix500to5000tags/formatter.py
@@ -1,40 +1,32 @@
-with open('K45791_01_17_2024_Comments.CSV') as comments:
-    with open('converted.csv', 'w') as w:
+with open(r'C:\Users\bburgess\Desktop\Python_Projects\logix500to5000tags\K45791_01_17_2024_Comments.CSV') as comments:
+    with open(r'C:\Users\bburgess\Desktop\Python_Projects\logix500to5000tags\converted.csv', 'w') as w:
         line = comments.readline()
 
-        # declare the curr for the current tag in a broader scope for later
         curr = -1
 
         while line != "":
-            # don't bother parsing lines we don't care about
+            
             if not line.startswith('I') and not line.startswith('O'):
-                line = comments.readline()  # have to move to the next line or infinite loop
+                line = comments.readline()  
                 continue
-
-            # split the contents of the line into a list we can use to make the right data
+            
             lineLst = line[:-1].split(',')
-
-            # if not using a / then drop it out of the thing
+            
             if '/' not in lineLst[0]:
                 line = comments.readline()
                 continue
-
-            # check to see if we've moved to a new tag or not
+            
             if (name := lineLst[0].split('/')[0]) != curr:
-                curr = name  # update the current tag we are within
-
+                curr = name  
                 tmp = name.split(':')
-
-                # write the tag line for the new tag we just found
+                
+                w.write('TAG,,Local:{slot}:"C","","AB:1756_D{addr}:"C":1","","(ExternalAccess := Read/Write)"\n'.format(slot=tmp[1], addr=tmp[0]))
                 w.write('TAG,,Local:{slot}:{addr},"","AB:1756_D{addr}:{addr}:0","","(ExternalAccess := Read/Write)"\n'.format(slot=tmp[1], addr=tmp[0]))
-
-            # build vars for the next format
+            
             tmp, terminal = lineLst[0].split('/')
             addr, slot = tmp.split(':')
             desc = lineLst[3]
-
-            # write the comment lines for it
+            
             w.write('COMMENT,,Local:{slot}:{addr},"{description}",,"Local:{slot}:{addr}.DATA.{terminal}"\n'.format(slot=slot, addr=addr, terminal=terminal, description=desc))
-
-            # move to the next line
+            
             line = comments.readline()
