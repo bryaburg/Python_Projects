6b510cfed94bd0e96d93c4f9c74461091eea7621
alloutnoob <bryanburgess95@hotmail.com>
2024-02-01 12:36:58 -0600

    added df info for help later

--- FTV_Tags/script.py
+++ FTV_Tags/script.py
@@ -2,46 +2,53 @@ import pandas as pd
 import re
 
 '''
-#Old style {datatype}:{array}.{bit}
-#New Style {datatype}{[array]}.{bit}
-
-Data Types 
-;Tag Type                 object
-Tag Name                  object
-Tag Description           object
-Read Only                 object
-Data Source               object
-Security Code             object
-Alarmed                   object
-Native Type               object
-Value Type                object
-Min Analog               float64
-Max Analog               float64
-Initial Analog           float64
-Scale                    float64
-Offset                   float64
-DeadBand                 float64
-Units                    float64
-Off Label Digital        float64
-On Label Digital         float64
-Initial Digital          float64
-Length String            float64
-Initial String           float64
-Retentive                float64
-Address                   object
-System Source Name       float64
-System Source Index      float64
-RIO Address              float64
-Element Size Block       float64
-Number Elements Block    float64
-Initial Block            float64
+RangeIndex: 352 entries, 0 to 351
+Data columns (total 29 columns):
+ #   Column                 Non-Null Count  Dtype
+---  ------                 --------------  -----
+ 0   ;Tag Type              352 non-null    object
+ 1   Tag Name               349 non-null    object
+ 2   Tag Description        2 non-null      object
+ 3   Read Only              349 non-null    object
+ 4   Data Source            349 non-null    object
+ 5   Security Code          349 non-null    object
+ 6   Alarmed                349 non-null    object
+ 7   Native Type            238 non-null    object
+ 8   Value Type             238 non-null    object
+ 9   Min Analog             238 non-null    float64
+ 10  Max Analog             238 non-null    float64
+ 11  Initial Analog         238 non-null    float64
+ 12  Scale                  238 non-null    float64
+ 13  Offset                 238 non-null    float64
+ 14  DeadBand               238 non-null    float64
+ 15  Units                  0 non-null      float64
+ 16  Off Label Digital      111 non-null    float64
+ 17  On Label Digital       111 non-null    float64
+ 18  Initial Digital        111 non-null    float64
+ 19  Length String          0 non-null      float64
+ 20  Initial String         0 non-null      float64
+ 21  Retentive              0 non-null      float64
+ 22  Address                349 non-null    object
+ 23  System Source Name     0 non-null      float64
+ 24  System Source Index    0 non-null      float64
+ 25  RIO Address            0 non-null      float64
+ 26  Element Size Block     0 non-null      float64
+ 27  Number Elements Block  0 non-null      float64
+ 28  Initial Block          0 non-null      float64
+dtypes: float64(19), object(10)
+memory usage: 79.9+ KB
+RangeIndex(start=0, stop=352, step=1)
 '''
 
 csv = r"C:\Users\bburgess\OneDrive - ESCO\Desktop\Python_Projects\FTV_Tags\Heinz_K45791-Tags.CSV"
 
 df = pd.read_csv(csv, sep=',', skipinitialspace=True)
 
-df = df.dropna(subset=['Address'])
+df_info = df.info()
+
+df_index = df.index
+
+df_row_address = df.loc[:, "Address"]
 
 regex_patterns = {
     'I': r'::\[[a-zA-Z0-9]+\]I:\d+/\d+',
@@ -59,7 +66,6 @@ regex_patterns = {
     'C5': r'::\[[a-zA-Z0-9]+\]C5:\d+\.ACC'
 }
 
-
 datatypes = {
     'I': {'array': [''], 'bit': ['']},
     'O': {'array': [''], 'bit': ['']},
@@ -73,8 +79,7 @@ datatypes = {
     'N26': {'array': [''], 'bit': ['']},
     'S': {'array': [''], 'bit': ['']},
     'T4': {'array': [''], 'bit': ['PRE']},
-    'C5': {'array': [''], 'bit': ['ACC']}   
-
+    'C5': {'array': [''], 'bit': ['ACC']}
 }
 
 N = 237
